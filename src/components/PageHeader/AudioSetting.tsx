1755dbc4a2fce37de2a30e661cd0a10a8bd29f45
Shobika Babu <shobika@sparkouttech.com>
2021-07-19 11:00:42 +0530

    Ui changes

--- src/components/PageHeader/AudioSetting.tsx
+++ src/components/PageHeader/AudioSetting.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Box, Flex, Text, PancakeToggle, useMatchBreakpoints } from '@pancakeswap-libs/uikit'
+import { Box, Flex, Text, useMatchBreakpoints } from '@pancakeswap-libs/uikit'
 import { useAudioModeManager } from 'state/user/hooks'
+import { PancakeToggle } from '../../uikit/index.cjs'
 
 type AudioSettingModalProps = {
   translateString: (translationId: number, fallback: string) => string
